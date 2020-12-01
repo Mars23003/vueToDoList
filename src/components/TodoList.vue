7733e64eccac26c6700b2e0a0f57392efc28e5d8
mars23003 <mars8602@gmail.com>
2020-12-01 17:22:40 +0800

    新增刪除功能

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,13 +2,18 @@
   <div>
     <h4 v-for="todo in $store.state.todo" :key="todo.id">
       {{todo.item}}
-      <button class="btn"></button>
+      <button class="btn" @click="delItem($index)">刪除</button>
     </h4>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoList"
+  name: "TodoList",
+  methods: {
+    delItem: function(index) {
+      this.$store.state.todo.splice(index, 1);
+    }
+  }
 };
 </script>
